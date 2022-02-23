417ff399e415191d02a049ca30c9ef3b272fd1f6
stesho01 <Listo4ek>
2022-02-23 18:34:15 +0200

    fix comments on Singleton

--- src/main/java/driver/SingletonDriver.java
+++ src/main/java/driver/SingletonDriver.java
@@ -1,23 +1,22 @@
 package driver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
 
 public class SingletonDriver {
     private static WebDriver driver;
 
     private SingletonDriver() {
-        WebDriverManager.chromedriver().setup();
-        driver.manage().deleteAllCookies();
-    };
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
 
     public static WebDriver getInstance() {
         if (driver == null) {
-            ChromeOptions options = new ChromeOptions();
-            options.setCapability("webdriver.chrome.driver", "src/main/resources/chromedriver");
-            driver = new ChromeDriver(options);
+            new SingletonDriver();
         }
         return driver;
     }
